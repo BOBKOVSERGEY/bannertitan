f8b976f95d94e2ca9986944f0e9d203acf16c74c
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-08-11 16:06:18 +0300

    Без полосы прокрутки

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,14 @@
 
 $(function(){
+  var elHtml = document.getElementsByTagName('body');
   setTimeout(function () {
     $('.wrap-banner').addClass('wrap-banner__active');
+    $(elHtml).addClass('body-sale-banner');
     return false;
   }, 1000);
   $('.wrap-banner__close').click(function (event) {
     event.preventDefault();
     $('.wrap-banner').removeClass('wrap-banner__active');
+    $(elHtml).removeClass('body-sale-banner');
   });
 });
